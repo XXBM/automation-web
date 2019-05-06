df0a8b115731fb0814a4c653c002f91a8f03276a
XXBM <1961346638@qq.com>
2019-05-06 17:36:03 +0800

    test

--- src/main/java/com/xg/hlh/automation_web/test/domain/AwardsRank.java
+++ src/main/java/com/xg/hlh/automation_web/test/domain/AwardsRank.java
@@ -12,8 +12,37 @@ public class AwardsRank implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.TABLE)
     private Long id;
+    private String description;
+    private String name;
+    public AwardsRank() {
+    }
+
     public AwardsRank(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
 
